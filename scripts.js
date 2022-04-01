7e1ebc3a2ddfa7923664327875efea4c63f4d73b
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-04-01 16:25:47 -0400

    The background of the shuttle is successfully changing to blue when clidking take off :)

--- scripts.js
+++ scripts.js
@@ -2,16 +2,20 @@
 // Remember to pay attention to page loading!
 function init () {
 
-    const takeoff = document.getElementById("takeoff")
+    const takeoff = document.getElementById("takeoff");
     const landing = document.getElementById("landing");
-
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
 
 
 //When the "Land" button is clicked, the following should happen:
 
 
     takeoff.addEventListener("click", function() {
-        console.log("first event listener test.")
+        console.log("first event listener test.");
+        flightStatus.innerHTML = "The shuttle has landed!";
+        shuttleBackground.style.backgroundColor = "blue";
+
     });
 
 };
