4fe362ffd8153479fe24b241872ec631f33b69ca
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-04-01 16:06:13 -0400

    first commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init () {
+
+    const takeoff = document.getElementById("takeoff")
+    const landing = document.getElementById("landing");
+
+
+
+//When the "Land" button is clicked, the following should happen:
+
+
+    takeoff.addEventListener("click", function() {
+        ale
+    })
+
+};
+
